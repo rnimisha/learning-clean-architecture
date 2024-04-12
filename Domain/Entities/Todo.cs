466247796ba72461e42082c38c88a5af19078c62
rnimisha <nimisaraut@gmail.com>
2024-04-12 19:40:32 +0545

    feat: add todo entity

---
+++ Domain/Entities/Todo.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Clean.Domain.Entities;
+
+public class Todo: BaseEntity{
+
+    [Required]
+    public required string Title {get; set;}
+
+    public bool IsCompleted {get; set;} = false;
+
+    [Required]
+    public required Guid User {get; set;} 
+
+}
